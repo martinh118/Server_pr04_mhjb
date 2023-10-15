bd4fbc5003150a3e511502c305491949f3f59731
martinh118 <94359581+martinh118@users.noreply.github.com>
2023-10-15 19:28:27 +0200

    Primera versión - Inicio sesión
    No funciona el cambio de paginas de los artículos.
    Funciona el cambio de página al de iniciar de sesión, y volver a reenviar al usuario a la página de artículos con el nombre de inicio de sesión.
    No hay comprobación de contraseña.

---
+++ practica_04_mhjb/model/modelo_martin_jaime.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * @author Martín Hernan Jaime Bonvin
+ * @version 1.0
+ */
+
+
+/**
+ * Crea la connexió a la base de dades i retorna la petició.
+ * @return connexio: objecte PDO amb la connexió directa a la base de dades.
+ */
+function conectar()
+{
+    try {
+        $connexio = new PDO('mysql:host=localhost;dbname=pt03_martin_jaime', 'root', '');
+        //echo "Connexio correcta!!" . "<br />";
+        return $connexio;
+    } catch (PDOException $e) { //
+        // mostrarem els errors
+        echo "Error: " . $e->getMessage();
+    }
+}
+?>
+
+<?php
+/**
+ * A partir de l'objecte PDO ($connexio) amb la connexió a la base de dades es crea una consulta SQL per seleccionar tot el contingut de la taula i s'executa.
+ * Guarda el resultat en $statement i retorna la informació.
+ * @param connexio: Paràmetre d'entrada amb les dades de la connexió a la base de dades.
+ * @return statement: Retorna la informació seleccionada a partir de la consulta SQL. 
+ *  
+ */
+function seleccionarTodo($connexio)
+{
+    try {
+        $statement = $connexio->prepare('SELECT * FROM articles');
+        $statement->execute();
+        return $statement;
+    } catch (PDOException $e) { //
+        // mostrarem els errors
+        echo "Error: " . $e->getMessage();
+    }
+}
+?>
+
+<?php
+/**
+ * A partir de l'objecte PDO ($connexio) amb la connexió a la base de dades es crea una consulta SQL per seleccionar els articles que hi ha entre un rang de IDs.
+ * Guarda el resultat en $statement i retorna la informació.
+ * Aquesta consulta és creada per mostrar els articles corresponents depenent la pàgina on estigui l'usuari.
+ * @param connexio: Paràmetre d'entrada amb les dades de la connexió a la base de dades.
+ * @param INICIO: Valor per agafar el primer ID de l'article a mostrar en la pàgina depenent on es trobi l'usuari. 
+ * @param FINAL: Valor per agafar l'últim ID de l'article a mostrar en la pàgina depenent on es trobi l'usuari.
+ * @return statement: Retorna la informació seleccionada a partir de la consulta SQL. 
+ *  
+ */
+function seleccionarRango($connexio, $INICIO, $FINAL)
+{
+    try {
+        $statement = $connexio->prepare('SELECT * FROM articles WHERE ID > :inicio AND ID <= :final');
+        $statement->execute(
+            array(
+                ':inicio' => $INICIO,
+                ':final' => $FINAL
+            )
+        );
+        return $statement;
+    } catch (PDOException $e) { //
+        // mostrarem els errors
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+
+
+?>
